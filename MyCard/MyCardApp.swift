76a254602892789e2fdeac698c188dfd81f527cc
absarrahman <raman040635@gmail.com>
2021-11-25 22:18:36 +0600

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by Moh. Absar Rahman on 25/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
